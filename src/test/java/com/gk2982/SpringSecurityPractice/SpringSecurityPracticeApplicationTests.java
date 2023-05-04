b9d18a4693bd78a53ffb5e9a5ffc22f208a82a98
Gourav Kumar <91111185+gourav2982@users.noreply.github.com>
2023-05-04 19:44:06 +0530

    empty commit

---
+++ src/test/java/com/gk2982/SpringSecurityPractice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk2982.SpringSecurityPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
